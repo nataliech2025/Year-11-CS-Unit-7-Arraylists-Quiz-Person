f14892fbdaa4ddea62c132c464cf84d0a483149f
Natalie Cheng <nataliech2025@student.cis.edu.hk>
2023-03-10 08:53:48 +0800

    CS.11.07 Quiz - IDE

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,33 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person (String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setFirstName(String x){
+        this.firstName = x;
+    }
+    public void setLastName(String y) {
+        this.lastName = y;
+    }
+    public void setAge(int z) {
+        this.age = z;
+    }
+
+    public String toString(){
+        return firstName + " " + lastName + ", " + age + "years old";
+    }
 }
--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -8,15 +8,39 @@ public class PersonFactory {
         allPersons = new ArrayList<>();
     }
 
-    public void addPerson(Person person) {
-
+    public void addPerson(Person person){
+        if (allPersons.size() == 0){
+            allPersons.add(person);
+            return;
+    }
+        int i = 0;
+        for (Person p : allPersons){
+            if (person.getLastName().compareTo(p.getLastName()) < 0){
+                allPersons.add(i, person);
+                break;
+            }
+            i++;
+        }
     }
 
     public ArrayList<Person> under18() {
+        ArrayList<Person> under18 = new ArrayList();
+        for(Person person1 : allPersons){
+            if(person1.getAge() < 18){
+                under18.add(person1);
+            }
+        }
+        return under18;
+    }
 
     }
 
-    public boolean checkLastName(String lastName) {
-
+    public boolean checkLastName(String lastName){
+        for(Person person1 : allPersons){
+            if(person1.getLastName().equals(lastName)){
+                return true;
+            }
+        }
+        return false;
     }
 }
